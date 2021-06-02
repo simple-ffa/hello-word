74b0ad2e89f2c60b7ba4627297f1e5853b8ed24c
fanfangan <fanfangan@baidu.com>
2021-06-02 19:48:43 +0800

    add ut case

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -1,8 +1,12 @@
-public class Test {
+public class HelloWord {
 
     public static void main(String[] args) {
         System.out.println("hello-world");
         System.out.println("hello-world");
         System.out.println("hello-world");
+    }
+
+    public static void sayHello() {
+        System.out.println("hello");
     }
 }
---
+++ src/test/java/HelloWordTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * @author fanfangan@baidu.com
+ **/
+
+public class HelloWordTest {
+
+    @Test
+    public void sayHello() {
+
+        HelloWord.sayHello();
+    }
+
+}
